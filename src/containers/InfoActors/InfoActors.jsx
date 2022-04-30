3507be491fc8f8c032e8963044cef81b3948fd6e
haCarrasco <Hugo.aCasstillo@gmail.com>
2022-04-30 08:24:09 -0500

    Aplica primeros estilos a componente ListMovies

--- src/containers/InfoActors/InfoActors.jsx
+++ src/containers/InfoActors/InfoActors.jsx
@@ -1,62 +1,91 @@
-import React from 'react'
-import { Component } from 'react';
+import React, { useEffect, useState } from "react";
 import { Card } from "antd";
-import { Row, Col, Layout } from 'antd'
+import { Row, Col, Layout } from "antd";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Divider } from 'antd';
+import { Button } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import  ListMovies  from "../../components/DragAndDropComponent/ListMovies";
 
 const { Content } = Layout;
 
+const InfoActors = () => {
+  const params = useParams();
+  const actorNameUnderscore = params.actorName;
+  const [actorInfo, setActorInfo] = useState({});
+  
+  
+  useEffect(() => {
+    const actorName = actorNameUnderscore.split("_").join(" ");
+    const getActorInfo = async () => {
+      const res = await axios.get(
+        `https://api.themoviedb.org/3/search/person?api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}&language=en-US&page=1&include_adult=false&query=${actorName}`
+      );
+      if (res.status === 200) {
+        const resActorInfo = res.data.results[0];
+        setActorInfo(resActorInfo);
+      }
+    };
 
-const { Meta } = Card;
+    getActorInfo();
+  }, [actorNameUnderscore]);
 
-class InfoActors extends Component  {
-   
-  render() {
-    return (
-      <div
-        className="info-container"
+  
+
+  return (
+    <div
+      className="info-container"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        width: "100%",
+        background: "#d9d9d9",
+        height: "800px",
+        padding: "70px 280px 0 80px",
+      }}
+    >
+      <Col
+        lg={{ span: 21 }}
+        xs={{ span: 8, offset: 2 }}
         style={{
-          background: "#d9d9d9",
-          height: "800px",
-          padding: "50px 0 0 170px",
+          background: "white",
+          width: "600px",
+          height: "600px",
+          justifyContent: "center",
+          paddingTop: "15px",
+          paddingRight: "0px",
         }}
       >
-        <Col
-          lg={{ span: 18, offset: 2 }}
-          xs={{ span: 8, offset: 2 }}
-          style={{
-            background: "white",
-            width: "800px",
-            height: "600px",
-            justifyContent: "center",
-            paddingTop: "40px",
-          }}
-        >
-          
-          <Content>
-            <Row>
-              <Col lg={{ span: 18, offset: 3 }} xs={{ span: 15, offset: 4 }}>
-                <Card
-                  hoverable
-                  style={{ width: 400, height: 200 }}
-                  cover={
-                    <img
-                      alt="example"
-                      src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                    />
-                  }
-                >
-                  <Meta
-                    title="Europe Street beat"
-                    description="www.instagram.com"
-                  />
-                </Card>
-              </Col>
-            </Row>
-          </Content>
-        </Col>
-      </div>
-    );
-  }
-}
+        <Content>
+          <Row className="head ant-col-24" style={{ display: "flex" }}>
+            <Button
+              className="btn"
+              type="primary"
+              style={{ margin: "10px 0 0px 20px" }}
+            >
+              <ArrowLeftOutlined /> Regresar
+            </Button>
+            <Divider />
+            <Card
+              hoverable
+              style={{
+                width: 200,
+                height: 250,
+                marginLeft: 60,
+                marginRight: -51,
+                border: "1px solid rgba(140, 140, 140, 0.35)",
+              }}
+              cover={<img alt="example" src={actorInfo.poster_path} />}
+            ></Card>
+            {actorInfo.id}
+
+            <ListMovies />
+          </Row>
+        </Content>
+      </Col>
+    </div>
+  );
+};
 
 export default InfoActors;
